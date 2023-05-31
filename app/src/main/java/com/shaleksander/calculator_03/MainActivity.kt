18a6bdd445bdfa0f8f38c27db50438dd412e11aa
AlexanderLexx <alexanderlexx@gmail.com>
2023-05-31 17:09:44 +0300

    add StringBuilder

--- app/src/main/java/com/shaleksander/calculator_03/MainActivity.kt
+++ app/src/main/java/com/shaleksander/calculator_03/MainActivity.kt
@@ -11,29 +11,36 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-    //numeric buttons
-    val zeroButton = findViewById<Button>(R.id.zero_button)
-    val oneButton = findViewById<Button>(R.id.one_button)
-    val twoButton = findViewById<Button>(R.id.two_button)
-    val threeButton = findViewById<Button>(R.id.three_button)
-    val fourButton = findViewById<Button>(R.id.four_button)
-    val fiveButton = findViewById<Button>(R.id.five_button)
-    val sixButton = findViewById<Button>(R.id.six_button)
-    val sevenButton = findViewById<Button>(R.id.seven_button)
-    val eightButton = findViewById<Button>(R.id.eigth_button)
-    val nineButton = findViewById<Button>(R.id.nine_button)
+        //numeric buttons
+        val zeroButton = findViewById<Button>(R.id.zero_button)
+        val oneButton = findViewById<Button>(R.id.one_button)
+        val twoButton = findViewById<Button>(R.id.two_button)
+        val threeButton = findViewById<Button>(R.id.three_button)
+        val fourButton = findViewById<Button>(R.id.four_button)
+        val fiveButton = findViewById<Button>(R.id.five_button)
+        val sixButton = findViewById<Button>(R.id.six_button)
+        val sevenButton = findViewById<Button>(R.id.seven_button)
+        val eightButton = findViewById<Button>(R.id.eigth_button)
+        val nineButton = findViewById<Button>(R.id.nine_button)
 
-    //operation button
-    val equalButton = findViewById<Button>(R.id.equal_button)
-    val plusButton = findViewById<Button>(R.id.plus_button)
-    val minusButton = findViewById<Button>(R.id.minus_button)
-    val multiplyButton = findViewById<Button>(R.id.multiply_button)
+        //operation button
+        val equalButton = findViewById<Button>(R.id.equal_button)
+        val plusButton = findViewById<Button>(R.id.plus_button)
+        val minusButton = findViewById<Button>(R.id.minus_button)
+        val multiplyButton = findViewById<Button>(R.id.multiply_button)
 
-    //text view
-    val resultTextView = findViewById<TextView>(R.id.result_textView)
-    resultTextView.text = "0"
+        //text view
+        val resultTextView = findViewById<TextView>(R.id.result_textView)
+
+        val numberStringBuilder = StringBuilder()
+
+        oneButton.setOnClickListener {
+            numberStringBuilder.append(1)
+            resultTextView.text = numberStringBuilder
+        }
 
     }
+
 
 
 
